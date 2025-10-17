f98c7583f4cd60c2abf3a7b37e3c3f359b53d389
Lucas Galli <luquigalli0207@gmail.com>
2025-10-17 12:01:14 -0300

    made player movement

--- Asteroids_LucasGalli/Main.cpp
+++ Asteroids_LucasGalli/Main.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    Vector2 pos = { 400, 225 };
+
+    float playerAngle = 0.0f;
+
+    Vector2 direction;
+
+    float radius = 50.0f;
+    float speed = 100.0f;
+
+    InitWindow(screenWidth, screenHeight, "Asteroids");
 
     SetTargetFPS(60);      
 
     while (!WindowShouldClose())  
     {
+        direction = Vector2Subtract(GetMousePosition(), pos);
+
+        direction.x = GetMouseX() - pos.x;
+        direction.y = GetMouseY() - pos.y;
+
+        Vector2 dirNormalizado = Vector2Normalize(direction);
+
+        if (direction.x > 0)
+        {
+            playerAngle = atan(direction.y / direction.x) * 60;
+        }
+        else
+        {
+            playerAngle = (atan(direction.y / direction.x) * 60) + 180;
+        }
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            pos.x += dirNormalizado.x * GetFrameTime() * speed;
+            pos.y += dirNormalizado.y * GetFrameTime() * speed;
+        }
 
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawPolyLines(pos,3,50,playerAngle,RAYWHITE);
 
         EndDrawing();
     }
 
     CloseWindow();     
 
-    return 0;    // Initialization
+    return 0;   
    
 }
