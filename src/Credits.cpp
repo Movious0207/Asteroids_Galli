d801a23f1f350b56dedf0f005b5ebdbd8faf245c
LucasGalli <luquigalli0207@gmail.com>
2025-12-04 05:47:07 -0300

    fixed credits

--- src/Credits.cpp
+++ src/Credits.cpp
@@ -3,8 +3,6 @@
 void Credits::Logic(GameState& screen, Vector2& mouse, Texture background)
 {
 
-	Texture credits = LoadTexture("res/credits.png");
-
 	while (screen == GameState::Credits)
 	{
 		int buttonSize = 70;
@@ -22,7 +20,14 @@ void Credits::Logic(GameState& screen, Vector2& mouse, Texture background)
 		BeginDrawing();
 		
 		DrawTextureEx(background, { 0,0}, 0, 0.9f, WHITE);
-		DrawTextureEx(credits, { -80, 80 }, 0, 0.9f, WHITE);
+
+		DrawText("CREDITS", screenWidth / 2 - 130, screenHeight / 11, 60, WHITE);
+		DrawText("ART", screenWidth / 2 -40, screenHeight / 5 + 50, 40, WHITE);
+		DrawText("All by Angie Tabasso (Estudiante primer ano Arte y animacion para videojuegos)", screenWidth / 8, screenHeight / 4 + 50, 20, WHITE);
+		DrawText("SOUND EFFECTS", screenWidth / 2 - 160, screenHeight / 3 + 70, 40, WHITE);
+		DrawText("All by miself using bfxr", screenWidth / 3 + 60, screenHeight / 3 + 120, 20, WHITE);
+		DrawText("MUSIC", screenWidth / 2 - 40, screenHeight / 2 + 70, 40, WHITE);
+		DrawText("Game and Menu Music by Gaston A-P from Pixabay", screenWidth / 4, screenHeight / 2 + 110, 20, WHITE);
 
 		DrawRectangle(10, 10, buttonSize, buttonSize, WHITE);
 		EndDrawing();
