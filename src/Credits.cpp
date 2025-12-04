3e362daf1dd623a8392147e19d8dfdee272f6da2
LucasGalli <luquigalli0207@gmail.com>
2025-12-04 15:40:59 -0300

    added links to credits and changed positioning

--- src/Credits.cpp
+++ src/Credits.cpp
@@ -21,13 +21,15 @@ void Credits::Logic(GameState& screen, Vector2& mouse, Texture background)
 		
 		DrawTextureEx(background, { 0,0}, 0, 0.9f, WHITE);
 
-		DrawText("CREDITS", screenWidth / 2 - 130, screenHeight / 11, 60, WHITE);
-		DrawText("ART", screenWidth / 2 -40, screenHeight / 5 + 50, 40, WHITE);
-		DrawText("All by Angie Tabasso (Estudiante primer ano Arte y animacion para videojuegos)", screenWidth / 8, screenHeight / 4 + 50, 20, WHITE);
-		DrawText("SOUND EFFECTS", screenWidth / 2 - 160, screenHeight / 3 + 70, 40, WHITE);
-		DrawText("All by miself using bfxr", screenWidth / 3 + 60, screenHeight / 3 + 120, 20, WHITE);
-		DrawText("MUSIC", screenWidth / 2 - 40, screenHeight / 2 + 70, 40, WHITE);
-		DrawText("Game and Menu Music by Gaston A-P from Pixabay", screenWidth / 4, screenHeight / 2 + 110, 20, WHITE);
+		DrawText("CREDITS", screenWidth / 2 - 130, screenHeight / 5, 60, WHITE);
+		DrawText("ART", screenWidth / 2 - 40, (screenHeight / 5) * 2, 40, WHITE);
+		DrawText("All by Angie Tabasso (Estudiante primer ano Arte y animacion para videojuegos) (itch: https://birritadulce.itch.io)", screenWidth / 8, static_cast<int>((screenHeight / 5) * 2.5f), 15, WHITE);
+		DrawText("SOUND EFFECTS", screenWidth / 2 - 160, (screenHeight / 5) * 3, 40, WHITE);
+		DrawText("All by miself using bfxr (https://www.bfxr.net)", screenWidth / 4, static_cast<int>((screenHeight / 5) * 3.5f), 20, WHITE);
+		DrawText("MUSIC", screenWidth / 2 - 60, (screenHeight / 5) * 4, 40, WHITE);
+		DrawText("Game and Menu Music by Gaston A-P from Pixabay ", screenWidth / 4, static_cast<int>((screenHeight / 5) * 4.3f), 20, WHITE);
+		DrawText("Game: https://pixabay.com/es/music/video-juegos-game-music-loop-19-153393/", screenWidth / 4 - 100, static_cast<int>((screenHeight / 5) * 4.5f), 20, WHITE);
+		DrawText("Menu: https://pixabay.com/es/music/video-juegos-game-music-loop-18-153392/", screenWidth / 4 - 100, static_cast<int>((screenHeight / 5) * 4.7f), 20, WHITE);
 
 		DrawRectangle(10, 10, buttonSize, buttonSize, WHITE);
 		EndDrawing();
